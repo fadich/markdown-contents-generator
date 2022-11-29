51a5052e3c4a27cdcf31697a0be45b422d05e0e3
Fadi A <fadich95@gmail.com>
2022-11-30 01:34:37 +0200

    Docs generation

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name="markdown_contents_generator",
-    version="0.1.7",
+    version="0.1.8",
     keywords=[
         "markdown-contents-generator",
         "md-contents-generator",
