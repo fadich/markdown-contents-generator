ed2f88047fc628d07cf1108b3058380fc00e2f24
Fadi A <fadich95@gmail.com>
2022-12-07 03:42:48 +0200

    Release v1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name="markdown_contents_generator",
-    version="0.2.0",
+    version="1.0.0",
     keywords=[
         "markdown-contents-generator",
         "md-contents-generator",
