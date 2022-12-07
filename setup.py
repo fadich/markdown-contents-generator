a2f7fa5cb61fd5272a423bf9908a79fb1f884ff0
Fadi A <fadich95@gmail.com>
2022-12-07 03:41:23 +0200

    Default margin set to four spaces

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name="markdown_contents_generator",
-    version="0.1.9",
+    version="0.2.0",
     keywords=[
         "markdown-contents-generator",
         "md-contents-generator",
